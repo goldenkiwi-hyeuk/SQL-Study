a15695ee043d80e34710b309cf5d493381fe7041
SeongHyeuk Park <95901686+goldenkiwi-hyeuk@users.noreply.github.com>
2024-11-05 20:46:06 +0900

    week 04 문제 풀이 추가 - 박성혁 (#16)
    week 04 문제 풀이 추가 - 박성혁

---
+++ week_04/parkseonghyeuk.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+
+SELECT ANIMAL_ID, ANIMAL_TYPE, NAME
+FROM ANIMAL_OUTS
+WHERE ANIMAL_ID in (
+    SELECT ANIMAL_ID 
+    FROM ANIMAL_INS
+    WHERE SEX_UPON_INTAKE IN ("Intact Male","Intact Female"))
+AND  SEX_UPON_OUTCOME IN ("Spayed Female", "Neutered Male");
